8f2e7769b4a2e6fc6251016eaf940f45de2c73d2
antik1801 <antik.edu@gmail.com>
2022-06-10 22:14:04 +0600

    number method

---
+++ tute20(Javascript Number Methods)/number_methods.js
@@ -0,0 +1,28 @@
+const a= {
+    toexponential: function ()
+    {
+        let x= 9;
+        console.log(x.toExponential(3));
+    },
+    tofix: function()
+    {
+        let x= 9.658845;
+        console.log(x.toFixed(3));
+    },
+    parsetint: function()
+    {
+        let x = 10.25658
+        console.log(parseInt(x));
+    },
+    property: function()
+    {
+        console.log(Number.MIN_VALUE);
+        console.log(Number.MAX_VALUE);
+        console.log(Number.POSITIVE_INFINITY);
+        console.log(Number.NEGATIVE_INFINITY);
+    }
+}
+a.toexponential();
+a.tofix();
+a.parsetint();
+a.property();

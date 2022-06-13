6acd70f35ccaf33f02ac83594f5b4a6e9ba83d09
antik1801 <antik.edu@gmail.com>
2022-06-13 20:28:28 +0600

    com

---
+++ tute23 -  array sort/array_sort.js
@@ -0,0 +1,33 @@
+const fruits = ["Banana","guava","apple","lemon","pineapple"];
+const number = [25,63,52,5,45,9,65,85];
+const cars = [
+    {name: "Volbo",year:2008},
+    {name: "Marcidis", year:2016},
+    {name: "suzuki", year: 2005},
+]
+console.log(number);
+const object = {
+    sortingNumbers: function()
+    {
+        number.sort(function(a,b){ return a - b; });
+    },
+    randomSorting: function()
+    {
+        console.log(number.sort(function(a,b){ return 0.5- Math.random();}));
+    },
+    mathmax: function()
+    {
+        console.log(this.mathmax.apply(number));
+    } ,
+    sortByYear: 
+
+}
+console.log(fruits.sort());
+// console.log(number.sort());
+console.log(fruits.reverse());
+object.sortingNumbers();
+console.log(number);
+console.log(number[0]);
+console.log(number[number.length - 1]);
+//object.mathmax();
+// object.randomSorting();

ec02e9de5929b69cb3657bf1e7942e99748b175a
antik1801 <antik.edu@gmail.com>
2022-07-09 17:33:57 +0600

    jh

---
+++ Js tuterial basic/tute 47 regular expression/regular_expression.js
@@ -0,0 +1,7 @@
+// Regular expression is used to search text pattern and text replace
+
+//------- /pattern/moddifier==> search()/ replace()
+
+let text = "visit w3  school"
+let n = text.search("w3");
+document.getElementById("demo").innerHTML= text.search("w3");

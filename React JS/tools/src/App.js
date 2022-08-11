891559599802fed6d27c723b1f52165ad7b50c19
antik1801 <antik.edu@gmail.com>
2022-08-11 21:40:08 +0600

    commit

---
+++ React JS/tools/src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Lets build the mern tinder</h1>
+  
+    {/*tinderCards*/}
+    {/*swipeButtons*/}
+    </div>
+  );
+}
+
+export default App;
